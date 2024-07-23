3cc97b858d35ba27a2e5e6ca1da66d5a5ce43e8a
Dongha <61765890+Modem924@users.noreply.github.com>
2024-07-23 13:59:29 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import {
   AppPaperScreen,
