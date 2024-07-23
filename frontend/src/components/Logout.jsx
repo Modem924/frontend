3317adb9442497a2a8093e21b2c2fb455b7b28f0
Dongha <61765890+Modem924@users.noreply.github.com>
2024-07-23 17:35:31 +0900

    Update Logout.jsx

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { userLogout } from "./api";
 
 const Logout = ({ setToken }) => {
   const navigate = useNavigate();
-  alert("로그아웃 하시겠습니까?");
 
-  userLogout();
-  setToken(null);
-  navigate("/");
+  useEffect(() => {
+    const confirmLogout = window.confirm("로그아웃 하시겠습니까?");
+    if (confirmLogout) {
+      userLogout();
+      setToken(null);
+      navigate("/");
+    } else {
+      navigate(-1); // 이전 페이지로 돌아갑니다.
+    }
+  }, [navigate, setToken]);
 
   return null;
 };
